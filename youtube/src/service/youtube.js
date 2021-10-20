b92d0a7df8ff3cdc9563b345830f98138dbaabe3
HongBeen <redbean-2@naver.com>
2021-10-20 20:31:45 +0900

    동영상 플레이어  16:9 비율 조정

--- youtube/src/service/youtube.js
+++ youtube/src/service/youtube.js
@@ -1,17 +1,23 @@
+import axios from "axios";
+
 class Youtube{
   constructor(key){
-    this.key = key;
-    this.requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
+    this.youtube = axios.create({
+      baseURL: "https://youtube.googleapis.com/youtube/v3",
+      params: { key },
+    });
   }
 
   async mostPopular(){
     try {
-      const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&maxResults=25&chart=mostPopular&key=${this.key} `, this.requestOptions);
-      const result = await response.json();
-      return result.items;
+      const response = await this.youtube.get('videos',{
+        params: {
+          part : 'snippet',
+          chart: 'mostPopular',
+          maxResults : 25,
+        }
+      });
+      return response.data.items;
     } catch (error) {
       return console.log('error', error);
     }
@@ -19,9 +25,16 @@ class Youtube{
 
   async search(query){
     try {
-      const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key} `, this.requestOptions);
-      const result = await response.json();
-      return result.items.map(item => ({...item, id: item.id.videoId}));
+      const response = await this.youtube.get('search',{
+        params: {
+          part : 'snippet',
+          chart: 'mostPopular',
+          q: query,
+          type: 'video',
+          maxResults : 25,
+        }
+      });
+      return response.data.items.map(item => ({...item, id: item.id.videoId}));
     } catch (error) {
       return console.log('error', error);
     }
