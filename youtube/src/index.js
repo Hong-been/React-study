77f3c1b6e46c6b5c9f91a7f1106a33a1c0be3dec
HongBeen <redbean-2@naver.com>
2021-10-22 00:53:36 +0900

    api key 숨김

--- youtube/src/index.js
+++ youtube/src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '@fortawesome/fontawesome-free/js/all.js';
-import './index.css';
-import App from './app';
-import Youtube from './service/youtube';
+import React from "react";
+import ReactDOM from "react-dom";
+import "@fortawesome/fontawesome-free/js/all.js";
+import "./index.css";
+import App from "./app";
+import Youtube from "./service/youtube";
 
-const key ="AIzaSyBUzWqXCNnWzk3LYPhmwXUMMEuTac0evrA";
+const key = process.env.REACT_APP_YOUTUE_API_KEY;
 const youtube = new Youtube(key);
 ReactDOM.render(
-  <React.StrictMode>
-    <App youtube={youtube}/>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App youtube={youtube} />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
-
