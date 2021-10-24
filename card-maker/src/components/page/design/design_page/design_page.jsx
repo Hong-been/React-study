27e9a571780416de13699ebe4f6ebedcadb6f2ba
HongBeen <redbean-2@naver.com>
2021-10-24 21:04:15 +0900

    로그인 후 사용자정보 받아서 보여주기

--- card-maker/src/components/page/design/design_page/design_page.jsx
+++ card-maker/src/components/page/design/design_page/design_page.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import styles from "./design_page.module.css";
 import Header from "../header/header";
 import MakerList from "../maker_list/maker_list";
+import { useLocation } from "react-router";
 
 const DesignPage = (props) => {
+	const { state } = useLocation();
+
 	const onLogoutClick = () => {
 		console.log("Click log-out button");
 	};
 	return (
 		<>
-			<Header onLogoutClick={onLogoutClick} />
+			<Header onLogoutClick={onLogoutClick} state={state} />
 			<MakerList />
 		</>
 	);
--- card-maker/src/components/page/design/header/header.jsx
+++ card-maker/src/components/page/design/header/header.jsx
@@ -1,13 +1,20 @@
 import React, { memo } from "react";
 import styles from "./header.module.css";
 
-const Header = memo((props) => {
+const Header = memo(({ state, onLogoutClick }) => {
+	const photoURL = state.photo;
+	const name = state.name;
+
 	return (
 		<header className={styles.header}>
 			<div className={styles.title}>Business Card Maker</div>
-			<button className={styles.logout} onClick={props.onLogoutClick}>
-				Log out
-			</button>
+			<div className={styles.user}>
+				<img className={styles.photo} src={photoURL}></img>
+				<span className={styles.name}>Welcome {name}!</span>
+				<button className={styles.logout} onClick={onLogoutClick}>
+					Log out
+				</button>
+			</div>
 		</header>
 	);
 });
